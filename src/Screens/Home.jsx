dc30ef763413ce81ab4835541820cc883a4a83f6
ranagoharayoub <ranagohar1163@gmail.com>
2021-08-22 00:23:11 +0500

    First Completion

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,16 +1,82 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core'
+import { AccountBalanceWallet, Close, ExpandMore } from '@material-ui/icons'
+import React, { useState } from 'react'
+import logo from '../Assets/Media/logo.svg'
 import './Home.css'
-import CloseIcon from '@material-ui/icons/Close';
+
 
 function Home() {
+
+    const [toggle, settoggle] = useState(false)
+
     return (
         <div className='home-cont'>
+
             <div className="page-profile">
-                <header>
-                    <CloseIcon></CloseIcon>
-                    {/* <span><img src="/Colour.png" alt="" /></span> */}
-                </header>
+
+               <div className="logo">
+
+                   <Close className='close' style={{cursor:'pointer'}}></Close>
+                   <img src={logo} alt='logo'></img>
+
+               </div>
+
+               <div className="content">
+
+                   <Avatar className='avatar' alt={'John Doe'} src={''}></Avatar>
+
+                   <div className="username">
+                       John Doe
+                   </div>
+
+                   <div className="wallet">
+                       <AccountBalanceWallet className='wallet-img'></AccountBalanceWallet>
+                       <div className="amount">
+                        ${'1,534'}
+                       </div>
+                   </div>
+
+                   <div >
+                       <div className="dashboard" >
+                            Dashboard
+                            <ExpandMore onClick={()=> settoggle(!toggle)} 
+                                        style={{cursor:'pointer'}}
+                                        className='close'
+                                        >                               
+                            </ExpandMore>
+                       </div>
+
+                        <div className="dropdowncont" style={toggle? {display:"flex"} : {display:'none'}}>
+                            <div className="portfolio">
+                                Portfolio
+                            </div>
+                            <div className="portfolio">
+                                Wallet 
+                            </div>
+                            <div className="portfolio">
+                                Membership
+                            </div>
+                        </div>
+                   </div>  
+
+                   
+
+                   <div className="property">
+                       Property Listing
+                   </div>
+
+                   <div className="property">
+                        About Us
+                   </div>
+
+                   <div className="property">
+                        FAQ
+                   </div>
+
+               </div>
+
             </div>
+
         </div>
     )
 }
