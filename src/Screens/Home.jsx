e0837862eeac8e788551f2a414d247676159d83b
ShahzaibChaudhry <81968966+ShahzaibChaudhry@users.noreply.github.com>
2021-08-21 21:39:45 +0500

    updated

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.css'
+import CloseIcon from '@material-ui/icons/Close';
 
 function Home() {
     return (
         <div className='home-cont'>
-            Home Page
+            <div className="page-profile">
+                <header>
+                    <CloseIcon></CloseIcon>
+                    {/* <span><img src="/Colour.png" alt="" /></span> */}
+                </header>
+            </div>
         </div>
     )
 }
